6c01cb6ed0ddff68fc5db542f7e731164b48778c
avash <avashshrestha14@augustana.edu>
2018-03-21 14:33:51 -0500

    minor changes

--- app/src/main/java/com/avashshrestha14/restaurantmenu/AppetizerFragment.java
+++ app/src/main/java/com/avashshrestha14/restaurantmenu/AppetizerFragment.java
@@ -1,26 +1,21 @@
 package com.avashshrestha14.restaurantmenu;
 
 import android.app.Fragment;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 
 public class AppetizerFragment extends Fragment{
-    private RadioGroup radioGroup;
-    private RadioButton radioButton;
 
+    private int selectedID;
+    private SharedPreferences prefs;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View myView = inflater.inflate(R.layout.fragment_appetizer, container, false);
-
-        radioGroup = (RadioGroup) myView.findViewById(R.id.radioGroup);
-        int selectedID = radioGroup.getCheckedRadioButtonId();
-        radioButton = (RadioButton) myView.findViewById(selectedID);
 
         return myView;
     }
